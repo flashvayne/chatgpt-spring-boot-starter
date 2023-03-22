ecc0f327f32da0ab545ee46ac9f80b64137277ff
vayne <flashvayne@gmail.com>
2023-03-22 11:29:26 +0800

    add more params for request

--- src/main/java/io/github/flashvayne/chatgpt/dto/ChatRequest.java
+++ src/main/java/io/github/flashvayne/chatgpt/dto/ChatRequest.java
@@ -1,22 +1,27 @@
 package io.github.flashvayne.chatgpt.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.Map;
+
+/**
+ * ChatRequest is used to construct request body.
+ * For descriptions of all fields, please refer to <a href="https://platform.openai.com/docs/api-reference/completions/create">Create completion</a>
+ */
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ChatRequest {
 
     private String model;
 
     private String prompt;
 
-    /**
-     * The maximum number of tokens to generate in the completion.
-     * The token count of your prompt plus max_tokens cannot exceed the model's context length.
-     * Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-     */
     @JsonProperty("max_tokens")
     private Integer maxTokens;
 
@@ -24,5 +29,39 @@ public class ChatRequest {
 
     @JsonProperty("top_p")
     private Double topP;
+
+    private String suffix;
+
+    private Integer n;
+
+    private Boolean stream;
+
+    private Integer logprobs;
+
+    private Boolean echo;
+
+    private String stop;
+
+    @JsonProperty("presence_penalty")
+    private Double presencePenalty;
+
+    @JsonProperty("frequency_penalty")
+    private Double frequencyPenalty;
+
+    @JsonProperty("best_of")
+    private Integer bestOf;
+
+    @JsonProperty("logit_bias")
+    private Map<String, Integer> logitBias;
+
+    private String user;
+
+    public ChatRequest(String model, String prompt, Integer maxTokens, Double temperature, Double topP) {
+        this.model = model;
+        this.prompt = prompt;
+        this.maxTokens = maxTokens;
+        this.temperature = temperature;
+        this.topP = topP;
+    }
 
 }
